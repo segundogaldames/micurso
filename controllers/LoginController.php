9b6e44b942c6b33dd323c034791bc38d71b7d2d5
Segundo Galdames <segundogaldames@gmail.com>
2025-12-20 01:02:14 -0300

    Creacion de pantallas de login e index publico

---
+++ controllers/LoginController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace controllers;
+
+use application\Controller;
+use application\Session;
+use application\Filter;
+use application\Flash;
+use application\Validate;
+use application\Password;
+use models\EmpresaPersona;
+use models\Usuario;
+use models\Persona;
+use models\User;
+
+final class LoginController extends Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index(): void
+    {
+        $this->redirect('login/login');
+    }
+
+    public function login()
+    {
+        if (Session::get('authenticate')) {
+            $this->redirect();
+        }
+
+        $this->_view->load('login/login', [
+            'titulo' => 'Login',
+            'subject' => 'Inicio de Sesión',
+            'send' => $this->encrypt($this->getForm()),
+            'process' => 'login/init',
+            'action' => 'create',
+        ]);
+    }
+
+    public function init()
+    {
+        if (Session::get('authenticate')) {
+            $this->redirect();
+        }
+
+        $fields = [
+            'email' => Filter::getPost('email'),
+            'password' => Filter::getPostRaw('password')
+        ];
+
+        $rules = [
+            'email' => 'required|email',
+            'password' => 'required'
+        ];
+
+        $this->validateForm('login/login', $fields, $rules);
+
+        $user = User::where('email', Filter::getPost('email'))
+            ->where('active', 1)
+            ->first();
+
+        if (!$user) {
+            Flash::error('El usuario no está registrado.');
+            $this->redirect('login/login');
+        }
+
+        if (!Password::verify(Filter::getPost('password'), $user->password)) {
+            Flash::error('Las credenciales ingresadas no son válidas');
+            $this->redirect('login/login');
+        }
+
+
+        Session::set('authenticate', true);
+        Session::set('user_id', $user->id);
+        Session::set('user_email', $user->email);
+        Session::set('user_name', $user->name);
+        Session::set('user_role', $user->role->name);
+        Session::set('time', time());
+
+        Flash::success('Bienvenido(a)');
+        $this->redirect('admin');
+    }
+
+    public function logout()
+    {
+        Session::destroy();
+        $this->redirect('login/login');
+    }
+}
